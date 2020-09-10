a44971e289d3f59e9ad4d7af60f71279f880e2f8
RishabhPandey <rishabhpandey644@gmail.com>
2020-09-10 14:19:44 +0530

    first commiting second time

--- first.py
+++ first.py
@@ -1,3 +1,6 @@
 print("First Python File in LrnGitRepo")
 for i in range(0,5):
     print(" Have done changes ",90,99)
+for i in range(0,5):
+    print("I will never give up")
+    print("I will work hard")
