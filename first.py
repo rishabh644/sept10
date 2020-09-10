3e20fd9c6a7cf1e2c1443c1959640b6e919d1434
RishabhPandey <rishabhpandey644@gmail.com>
2020-09-10 16:11:39 +0530

    Commited on dev

--- first.py
+++ first.py
@@ -5,3 +5,7 @@ for i in range(0,5):
     print("I will never give up")
     print("I will work hard")
 print("I have changed branch to dev from master")
+for i in range(0,5):
+    print("I will develop android app web app machine learning app django app node js app angular js app")
+    
+    
